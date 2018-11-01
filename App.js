92f023ae136542c823b17598a918e22080980980
KimHyunsu <khszone02@gmail.com>
2018-11-01 23:07:26 +0900

    firebase api tested

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import fb from './utils/firebase';
 
 export default class App extends React.Component {
   constructor() {
     super();
-    fb.init();
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+
     );
   }
 }
-
-const styles = StyleSheet.create({
-  
-});
