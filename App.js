3cc1cad5300ac91b720c88a7e044dcf614f8ddfd
KimHyunsu <khszone02@gmail.com>
2018-11-05 21:31:23 +0900

    meaningless testing

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
+import nlp from './utils/nlp.js';
 
 export default class App extends React.Component {
   constructor() {
     super();
+  }
+  
+  componentDidMount() {
+    nlp.meaningless("만 6세 이하의 유치원생")
   }
 
   render() {
--- utils/nlp.js
+++ utils/nlp.js
@@ -1,26 +1,64 @@
 import axios from "axios";
-// TODO: change request to axios
+
 export default nlp = {
-  meaningless: async function (text) {
+  analyse: async function (text) {
     try {
       const {data} = await axios.get(`http://parser.datanada.com/parse?version=1&string=${text}`);
+      return data
+    } catch (e) {
+      console.log(e.toString())
+      throw e;
+    }
+  },
+  meaningless: async function (text) {
+    try {
+      const morphemes = await this.analyse(text)
       let noun = 0;
       let verb = 0;
-      data.forEach((morpheme) => {
+      let previous = {
+        "deprel": "",
+        "pos": "",
+        "form": "",
+        "head": "",
+        "id": ""
+      };
+      console.log(morphemes)
+      morphemes.forEach((morpheme) => {
         let pos = morpheme["pos"]
+        if (morpheme["deprel"] == previous["deprel"]) {
+          return;
+        }
         if (pos.indexOf('N') == 0) {
           // 명사
           noun++;
         }
-        if (pos.indexOf('VV') == 0) {
+        if (pos == 'VV') {
           // 동사
           verb++;
         }
+        previous = morpheme
       });
       if (noun > 1 && verb > 0) {
+        console.log("의미 있는 문장")
         return false;
       }
+      console.log("의미 없는 문장")
       return true;
+    } catch (e) {
+      console.log(e.toString());
+      throw e;
+    }
+  },
+  test: async function (text) {
+    try {
+      const morphemes = await this.analyse(text)
+      let sentences = 0;
+      morphemes.forEach((morpheme) => {
+        if (morpheme["deprel"] == "ROOT") {
+          sentences++
+        }
+      });
+      console.log(sentences)
     } catch (e) {
       console.log(e.toString());
       throw e;
