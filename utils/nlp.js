1c8f70b3155cb869c7781551dc950ec85ed89dcc
KIONLEE <>
2018-11-09 03:00:22 +0900

    update nlp with several more features

--- utils/nlp.js
+++ utils/nlp.js
@@ -1,13 +1,32 @@
 import axios from "axios";
-// TODO: change request to axios
+
 export default nlp = {
-  meaningless: async function (text) {
+  analyse: async function (text) {
     try {
       const {data} = await axios.get(`http://parser.datanada.com/parse?version=1&string=${text}`);
+      return data
+    } catch (e) {
+      console.log(e.toString())
+      throw e;
+    }
+  },
+  isMeaningful: async function (text) {
+    try {
+      const morphemes = await this.analyse(text)
       let noun = 0;
       let verb = 0;
-      data.forEach((morpheme) => {
+      let previous = {
+        "deprel": "",
+        "pos": "",
+        "form": "",
+        "head": "",
+        "id": ""
+      };
+      morphemes.forEach((morpheme) => {
         let pos = morpheme["pos"]
+        // if (morpheme["deprel"] == previous["deprel"]) {
+        //   return;
+        // }
         if (pos.indexOf('N') == 0) {
           // 명사
           noun++;
@@ -16,11 +35,33 @@ export default nlp = {
           // 동사
           verb++;
         }
+        previous = morpheme
       });
       if (noun > 1 && verb > 0) {
-        return false;
+        return true;
       }
-      return true;
+      return false;
+    } catch (e) {
+      console.log(e.toString());
+      throw e;
+    }
+  },
+  isNotEmpty: function (text) {
+    if (!text.trim()) {
+      return false
+    }
+    return true
+  },
+  test: async function (text) {
+    try {
+      const morphemes = await this.analyse(text)
+      let sentences = 0;
+      morphemes.forEach((morpheme) => {
+        if (morpheme["deprel"] == "ROOT") {
+          sentences++
+        }
+      });
+      console.log(sentences)
     } catch (e) {
       console.log(e.toString());
       throw e;
