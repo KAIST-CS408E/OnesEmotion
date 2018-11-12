79455ea83134df87137912f5eb46b81156b20497
KimHyunsu <khszone02@gmail.com>
2018-11-12 22:32:23 +0900

    nlp merged

--- utils/nlp.js
+++ utils/nlp.js
@@ -1,12 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-export default nlp = {
-  analyse: async function (text) {
-    try {
-      const {data} = await axios.get(`http://parser.datanada.com/parse?version=1&string=${text}`);
-      return data
-=======
 import {ETRI_ACCESS_KEY} from '../configs/nlpConfig';
 
 export default nlp = {
@@ -20,38 +13,11 @@ export default nlp = {
         })
       })
       return morphemes
->>>>>>> a7a07f5dea5e7782daf5cca0faf2170e12214f33
     } catch (e) {
       console.log(e.toString())
       throw e;
     }
   },
-<<<<<<< HEAD
-  isMeaningful: async function (text) {
-    try {
-      const morphemes = await this.analyse(text)
-      let noun = 0;
-      let verb = 0;
-      let previous = {
-        "deprel": "",
-        "pos": "",
-        "form": "",
-        "head": "",
-        "id": ""
-      };
-      morphemes.forEach((morpheme) => {
-        let pos = morpheme["pos"]
-        // if (morpheme["deprel"] == previous["deprel"]) {
-        //   return;
-        // }
-        if (pos.indexOf('N') == 0) {
-          // 명사
-          noun++;
-        }
-        if (pos.indexOf('V') == 0) {
-          // 동사
-          verb++;
-=======
   analyseByEtri: async function (text) {
     try {
       const {data} = await axios.post('http://aiopen.etri.re.kr:8000/WiseNLU', {
@@ -59,9 +25,7 @@ export default nlp = {
         'argument': {
             'text': text,
             'analysis_code': 'ner'
->>>>>>> a7a07f5dea5e7782daf5cca0faf2170e12214f33
         }
-        previous = morpheme
       });
       let morphemes = [];
       data.return_object.sentence.forEach((sentence) => {
