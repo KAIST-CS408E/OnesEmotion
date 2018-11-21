a1dadfeef175bb03d2001e57e12fc4231c480ce2
Nyoungwoo <leenw2@kaist.ac.kr>
2018-11-22 03:23:18 +0900

    add images

--- assets/Images/getBackgroundImageName.js
+++ assets/Images/getBackgroundImageName.js
@@ -1,5 +1,5 @@
 export default function getBackgroundImageName() {
-	var maxNumOfImage = 4;
+	var maxNumOfImage = 10;
   var num = Math.floor(Math.random() * (maxNumOfImage)) + 1;
 	num += "";
 	const imagename = "background" + num;
